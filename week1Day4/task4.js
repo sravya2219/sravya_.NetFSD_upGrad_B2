775c8e592e84726a55ddda338255c66b78e0a1c8
sravya <yaragorlasravya@gmail.com>
2026-02-19 16:10:45 +0530

    Add files via upload

---
+++ week1Day4/task4.js
@@ -0,0 +1,14 @@
+let num=3;
+let res=(num > 0) ? "positive" : "negative";
+console.log(res);
+
+if(num%2===0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}
+
+for(let i=1;i<=num;i++){
+    console.log(i);
+}
