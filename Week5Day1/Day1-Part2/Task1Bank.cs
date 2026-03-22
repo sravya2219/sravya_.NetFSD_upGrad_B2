efd88ea58fc0d87f44e92fa7dffff6e5378f501a
sravya <yaragorlasravya@gmail.com>
2026-03-22 13:46:17 +0530

    Add files via upload

---
+++ Week5Day1/Day1-Part2/Task1Bank.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecisionMakingConstructors.HandsOn.WEEK5.Day2
+{
+    internal class Task1Bank
+    {
+        private string accountNumber;
+        private double balance;
+        public  string AccountNumber { get; set; }
+        public  double Balance { get; set; }
+        public  void Deposite(double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Invalid deposit amount!");
+                return;
+            }
+            else
+            {
+                balance += amount;
+                Console.WriteLine($"Deposited :{amount}");
+                Console.WriteLine($"Current balance:{balance}");
+            }
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Invalid withdraw amount!");
+                return;
+            }
+            else
+            {
+                balance -= amount;
+                Console.WriteLine($"withdraw :{amount}");
+                Console.WriteLine($"Current balance:{balance}");
+            }
+        }
+        static void Main()
+        {
+            Task1Bank bank = new Task1Bank();
+            bank.AccountNumber = "12345";
+            bank.Deposite(5000);
+            bank.Withdraw(2000);
+        }
+    }
+}
---
+++ Week5Day1/Day1-Part2/Task2Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecisionMakingConstructors.HandsOn.WEEK5.Day2
+{
+      class Employee
+    {
+        public string Name { get; set; }
+        public double BaseSalary { get; set; }
+       public Employee(string name,double baseSalary)
+        {
+            Name = name;
+            BaseSalary = baseSalary;
+        }
+        public virtual double CalculateSalary()
+        {
+            return BaseSalary;
+        }
+    }
+    class Manager : Employee
+    {
+       public  Manager(string Name, double BaseSalary) : base(Name, BaseSalary) { }
+        public override double CalculateSalary()
+        {
+            return BaseSalary + (BaseSalary * 20/100);
+        }
+    }
+    class Developer : Employee
+    {
+       public Developer(string Name, double BaseSalary) : base(Name, BaseSalary) { }
+        public override double CalculateSalary()
+        {
+            return BaseSalary + (BaseSalary * 10 / 100);
+        }
+    }
+    internal class Task2Employee
+    {
+        static void Main()
+        {
+            Employee e;
+            e = new Manager("jhon", 50000);
+            Console.WriteLine("Manager Salary = " + e.CalculateSalary());
+            e = new Developer("sara", 50000);
+            Console.WriteLine("Manager Salary = " + e.CalculateSalary());
+        }
+    }
+}
