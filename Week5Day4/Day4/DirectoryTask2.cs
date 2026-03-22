9dbe80915ebb896c51d7caafbf377c6c459db4f6
sravya <yaragorlasravya@gmail.com>
2026-03-22 13:49:24 +0530

    Add files via upload

---
+++ Week5Day4/Day4/DirectoryTask2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecisionMakingConstructors.HandsOn.WEEK5.Day4
+{
+    internal class DirectoryTask2
+    {
+        static void Main()
+        {
+            string path = "@C:\\Test";
+            if (Directory.Exists(path))
+            {
+                string[] files = Directory.GetFiles(path);
+                int count = 0;
+                foreach (var file in files)
+                {
+                    FileInfo fi = new FileInfo(file);
+                    Console.WriteLine(fi.Name);
+                    Console.WriteLine(fi.Length);
+                    Console.WriteLine(fi.CreationTime);
+                    count++;
+                }
+                Console.WriteLine($"number of files in the folder: {count}");
+            }
+            else
+            {
+                Console.WriteLine("invalid folder path");
+            }
+        }
+    }
+}
---
+++ Week5Day4/Day4/TuplesandPatterMatchingTask3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecisionMakingConstructors.HandsOn.WEEK5.Day4
+{
+    internal class TuplesandPatterMatchingTask3
+    {
+        static void Main()
+        {
+            Console.Write("Employee Name:");
+            var name = Console.ReadLine();
+            Console.Write("sales Amount:");
+            var amount = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Rating:");
+            var rate = Convert.ToInt32(Console.ReadLine());
+           
+
+            var result = GetPerformance(amount, rate);
+
+            //pattern matching
+            string performance = result switch
+            {
+                ( >= 100000, >= 4) => "High Performance",
+                ( >= 50000, >= 3) => "Average Performance",
+                _ => "Need Improvement"
+            };
+            Console.WriteLine("\nEmployee Name: " + name);
+            Console.WriteLine("Sales Amount: " + result.amount);
+            Console.WriteLine("Rating: " + result.rate);
+            Console.WriteLine("Performance: " + performance);
+
+        }
+        public static (double amount,int rate) GetPerformance(double amount,int rate)
+        {
+            return (amount,rate);
+        }
+    }
+}
