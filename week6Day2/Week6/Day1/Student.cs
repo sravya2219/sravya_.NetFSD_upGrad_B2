b84886975e09fa269cd9a082f18ef2d84a65137f
sravya <yaragorlasravya@gmail.com>
2026-03-24 22:44:48 +0530

    Add files via upload

---
+++ week6Day2/Week6/Day1/Student.cs
@@ -0,0 +1,69 @@
+﻿using DecisionMakingConstructors.collections;
+using System.Collections.Generic;
+using System;
+
+
+namespace DecisionMakingConstructors.HandsOn.Week6.Day1
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Marks { get; set; }
+
+    }
+    // manage data
+    public class StudentRepository
+    {
+        private List<Student> students = new List<Student>();
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+        }
+        public List<Student> GetAllStudents()
+        {
+            return students;
+        }
+    }
+    public class ReportGenerator
+    {
+        public void GenerateReport(List<Student> students)
+        {
+            Console.WriteLine("----- Student Report -----");
+
+            foreach (var student in students)
+            {
+                string grade;
+
+                if (student.Marks >= 90)
+                    grade = "A";
+                else if (student.Marks >= 75)
+                    grade = "B";
+                else if (student.Marks >= 50)
+                    grade = "C";
+                else
+                    grade = "Fail";
+
+                Console.WriteLine($"ID: {student.StudentId}");
+                Console.WriteLine($"Name: {student.StudentName}");
+                Console.WriteLine($"Marks: {student.Marks}");
+                Console.WriteLine($"Grade: {grade}");
+                Console.WriteLine("--------------------------");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            StudentRepository repo = new StudentRepository();
+
+            repo.AddStudent(new Student { StudentId = 1, StudentName = "Sravya", Marks = 92 });
+            repo.AddStudent(new Student { StudentId = 2, StudentName = "Ravi", Marks = 67 });
+            repo.AddStudent(new Student { StudentId = 3, StudentName = "Anu", Marks = 45 });
+
+            ReportGenerator report = new ReportGenerator();
+            report.GenerateReport(repo.GetAllStudents());
+        }
+    }
+}
---
+++ week6Day2/Week6/Day1/Task3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecisionMakingConstructors.HandsOn.Week6.Day1
+{
+    public abstract class Shape 
+    {
+        public abstract double CalculateArea();
+
+    }
+    public class Rectangle: Shape
+    {
+         public double Length { get; set; }
+        public double Width { get; set; }
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+        public override double CalculateArea()
+        {
+            return Length * Width;
+        }
+    }
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+    class Program2
+    {
+        static void Main()
+        {
+            Shape s1 = new Rectangle(10,20);
+            Console.WriteLine(s1.CalculateArea());
+            Shape s2 = new Circle(10);
+            Console.WriteLine(s2.CalculateArea());
+        }
+    }
+}
