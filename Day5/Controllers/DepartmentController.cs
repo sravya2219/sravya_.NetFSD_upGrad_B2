89420094392b4e7b39f5510b41362dd1655c3ce6
sravya <yaragorlasravya@gmail.com>
2026-04-16 13:30:00 +0530

    Add files via upload

---
+++ Day5/Controllers/DepartmentController.cs
@@ -0,0 +1,33 @@
+﻿using DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public DepartmentController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // ✅ GET ALL
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_context.Departments.ToList());
+        }
+
+        // ✅ ADD
+        [HttpPost]
+        public IActionResult Create(Department dept)
+        {
+            _context.Departments.Add(dept);
+            _context.SaveChanges();
+            return Ok(dept);
+        }
+    }
+}
---
+++ Day5/DataAccess/IContactRepo.cs
@@ -0,0 +1,16 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.DataAccess
+{
+    public interface IContactRepo
+    {
+        Task<List<ContactInfo>> GetAllAsync();
+        Task<ContactInfo?> GetByIdAsync(int id);
+        Task<ContactInfo> AddAsync(ContactInfo contact);
+        Task<bool> UpdateAsync(int id, ContactInfo contact);
+        Task<bool> DeleteAsync(int id);
+    }
+}
